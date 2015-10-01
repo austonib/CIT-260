3c5ccb906b145520321306fcb27629b2ad25d9fe
austonib <lee13011@byui.edu>
2015-09-30 18:18:48 -0600

    I just added a practice page for Printf()

---
+++ PrintFPractice.java
@@ -0,0 +1,19 @@
+
+/* 
+* @Author: austinlee
+* @Date:   2015-09-24 15:22:09
+* @Last Modified by:   austinlee
+* @Last Modified time: 2015-09-30 12:16:10
+*/
+
+import java.util.Scanner;
+
+public class PrintFPractice {
+    public static void main(String[] args) {
+    	Scanner input = new Scanner(System.in);
+
+    	double userIn = input.nextDouble();
+
+    	System.out.println(userIn);
+    }
+}
